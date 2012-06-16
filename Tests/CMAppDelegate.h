44c6a06ccbe733b93314a9eb59c94fdded476b8e
Tim Duckett <tim@charismaticmegafauna.co.uk>
2012-06-16 15:35:10 +0100

    Added basic GHUnit tests of dummy data creation

---
+++ Tests/CMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAppDelegate.h
+//  Tests
+//
+//  Created by Tim Duckett on 16/06/2012.
+//  Copyright (c) 2012 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
